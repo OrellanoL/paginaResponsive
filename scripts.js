52b971282a470fbd2451a77328d218423d710bce
OrellanoL <89207845+OrellanoL@users.noreply.github.com>
2023-06-15 15:25:40 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var desplegarMenu= document.getElementById("desplegar");
+
+desplegarMenu.addEventListener("click", ()=>{
+
+    document.getElementById("desplegable").style.top="1.2em";
+    document.getElementById("desplegable").style.opacity="1";
+    document.getElementById("desplegar").style.display="none";
+    
+});
